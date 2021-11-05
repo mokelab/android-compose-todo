f3e058385df7f8451658edf4805ad718f4b459c0
Hirokazu Fukami <fkm@mokelab.com>
2021-11-05 11:01:27 +0900

    Add Navigation compose

--- app/src/main/java/com/mokelab/compose/todo/ToDoApp.kt
+++ app/src/main/java/com/mokelab/compose/todo/ToDoApp.kt
@@ -3,14 +3,23 @@ package com.mokelab.compose.todo
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.mokelab.compose.todo.ui.theme.ComposeToDoTheme
 
 @Composable
 fun ToDoApp() {
+    val navController = rememberNavController()
+
     ComposeToDoTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-            Greeting("Android")
+        NavHost(navController = navController, startDestination = "main") {
+            composable("main") {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
         }
     }
 }
