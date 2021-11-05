282173128cd3492717018cd15bcc5bf13e3571d3
Hirokazu Fukami <fkm@mokelab.com>
2021-11-05 20:06:31 +0900

    Add screen composables

---
+++ app/src/main/java/com/mokelab/compose/todo/ui/detail/ToDoDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.mokelab.compose.todo.ui.detail
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ToDoDetailScreen(navController: NavController, todoId: Int) {
+
+}
