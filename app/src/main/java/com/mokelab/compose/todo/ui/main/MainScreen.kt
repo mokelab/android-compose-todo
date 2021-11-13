593b3dcd611bb6d15cccc679288a3872e3ddc713
Hirokazu Fukami <fkm@mokelab.com>
2021-11-13 14:17:03 +0900

    Add Scaffold

--- app/src/main/java/com/mokelab/compose/todo/ui/main/MainScreen.kt
+++ app/src/main/java/com/mokelab/compose/todo/ui/main/MainScreen.kt
@@ -1,18 +1,55 @@
 package com.mokelab.compose.todo.ui.main
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
-import com.mokelab.compose.todo.Greeting
+import androidx.navigation.compose.rememberNavController
+import com.mokelab.compose.todo.R
+import com.mokelab.compose.todo.ui.theme.ComposeToDoTheme
 
 @Composable
 fun MainScreen(
     navController: NavController,
     viewModel: MainViewModel,
 ) {
-    // A surface container using the 'background' color from the theme
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+    Scaffold(
+        topBar = { MainTopBar() },
+        floatingActionButton = { MainFAB(navController) }
+    ) {
+        // ToDoのリスト表示をここに書く
+    }
+}
+
+@Composable
+fun MainTopBar() {
+    TopAppBar(
+        title = {
+            Text(stringResource(id = R.string.app_name))
+        },
+    )
+}
+
+@Composable
+fun MainFAB(navController: NavController) {
+    FloatingActionButton(onClick = {
+        // 作成画面へ画面遷移
+        navController.navigate("create")
+    }) {
+        Icon(Icons.Filled.Add, "Add")
+    }
+}
+
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    val navController = rememberNavController()
+    val viewModel = MainViewModel()
+    ComposeToDoTheme {
+        MainScreen(navController = navController, viewModel = viewModel)
     }
 }
