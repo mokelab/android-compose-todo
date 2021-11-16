203d737a5be481622205b87a89b580c5bd6f9ada
Hirokazu Fukami <fkm@mokelab.com>
2021-11-16 11:39:36 +0900

    Implement lazycolumn

--- app/src/main/java/com/mokelab/compose/todo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/mokelab/compose/todo/ui/main/MainViewModel.kt
@@ -1,8 +1,13 @@
 package com.mokelab.compose.todo.ui.main
 
 import androidx.lifecycle.ViewModel
+import com.mokelab.compose.todo.repository.todo.ToDoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel()
+class MainViewModel @Inject constructor(
+    private val repo: ToDoRepository
+) : ViewModel() {
+    val todoList = repo.getAll()
+}
