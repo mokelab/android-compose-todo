82aeece5053f182dc9f2ede6b390afffea5ca52f
Hirokazu Fukami <fkm@mokelab.com>
2021-11-20 00:48:17 +0900

    Use MutableStateFlow

--- app/src/main/java/com/mokelab/compose/todo/ui/detail/ToDoDetailViewModel.kt
+++ app/src/main/java/com/mokelab/compose/todo/ui/detail/ToDoDetailViewModel.kt
@@ -4,20 +4,23 @@ import androidx.lifecycle.ViewModel
 import com.mokelab.compose.todo.model.todo.ToDo
 import com.mokelab.compose.todo.repository.todo.ToDoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class ToDoDetailViewModel @Inject constructor(
     private val repo: ToDoRepository
 ) : ViewModel() {
-    private lateinit var _todo: Flow<ToDo>
-    val todo: Flow<ToDo> get() = _todo
+    private val todoId = MutableStateFlow(-1)
+
+    @ExperimentalCoroutinesApi
+    val todo: Flow<ToDo> = todoId.flatMapLatest { todoId -> repo.getById(todoId) }
 
     fun setId(todoId: Int) {
-        _todo = repo.getById(todoId)
+        this.todoId.value = todoId
     }
 
 }
