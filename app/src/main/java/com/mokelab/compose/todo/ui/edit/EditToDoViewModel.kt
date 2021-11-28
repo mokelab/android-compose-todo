bf3e89889f30af36069a39a6f983ed2f04f2a759
Hirokazu Fukami <fkm@mokelab.com>
2021-11-28 13:44:27 +0900

    fix ViewModel

--- app/src/main/java/com/mokelab/compose/todo/ui/edit/EditToDoViewModel.kt
+++ app/src/main/java/com/mokelab/compose/todo/ui/edit/EditToDoViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.mokelab.compose.todo.model.todo.ToDo
 import com.mokelab.compose.todo.repository.todo.ToDoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,14 +16,16 @@ import javax.inject.Inject
 class EditToDoViewModel @Inject constructor(
     private val repo: ToDoRepository
 ) : ViewModel() {
-    private lateinit var _todo: Flow<ToDo>
-    val todo: Flow<ToDo> get() = _todo
+    private val todoId = MutableStateFlow(-1)
+
+    @ExperimentalCoroutinesApi
+    val todo: Flow<ToDo> = todoId.flatMapLatest { todoId -> repo.getById(todoId) }
 
     val errorMessage = MutableStateFlow("")
     val done = MutableStateFlow(false)
 
     fun setId(todoId: Int) {
-        _todo = repo.getById(todoId)
+        this.todoId.value = todoId
     }
 
     fun save(todo: ToDo, title: String, detail: String) {
